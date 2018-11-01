04de6922031ff873e47fdde0f78071a1634f9998
RunninglVlan <RedCapShortCut@gmail.com>
2018-11-01 21:58:59 +0200

    Wrote some instructions for the player   Created new script, attached it to game object     Created folder for scripts   Wrote strings to terminal using verbatim string literal     Enabled latest scriptingRuntimeVersion for that

---
+++ 02-TerminalHacker/Assets/Scripts/Hacker.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+
+    void Start()
+    {
+        Terminal.WriteLine(
+@"What would you like to hack into?
+
+Press 1 for the local library
+Press 2 for the police station
+Press 3 for NASA
+
+Enter your selection:");
+    }
+}
