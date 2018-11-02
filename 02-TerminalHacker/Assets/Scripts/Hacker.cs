a02529806810b68b0a4898c4546d65e57c435ad0
RunninglVlan <RedCapShortCut@gmail.com>
2018-11-02 18:14:53 +0200

    Added password checking   Modified List constant to be Dictionary with levels and passwords   Fixed problem that after showing menu we were still in password state   Increased charactersHigh to 13   Changed wording a bit again

--- 02-TerminalHacker/Assets/Scripts/Hacker.cs
+++ 02-TerminalHacker/Assets/Scripts/Hacker.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Hacker : MonoBehaviour
 {
 
-    private static readonly List<string> SUPPORTED_LEVELS = new List<string> { "1", "2", "3" };
+    private static readonly Dictionary<string, string> LEVEL_PASSWORDS = new Dictionary<string, string> {
+        { "1", "bookworm" }, { "2", "witness" }, { "3", "apollo" }
+    };
 
     private int level;
-    private Screen screen = Screen.MainMenu;
+    private Screen screen;
 
     void Start()
     {
@@ -17,6 +19,7 @@ public class Hacker : MonoBehaviour
 
     private void showMainMenu()
     {
+        screen = Screen.MainMenu;
         Terminal.ClearScreen();
         Terminal.WriteLine(
 @"What would you like to hack into?
@@ -38,11 +41,15 @@ Enter your selection:");
         {
             processMainMenu(input);
         }
+        else if (screen == Screen.Password)
+        {
+            checkPassword(input);
+        }
     }
 
     private void processMainMenu(string input)
     {
-        if (SUPPORTED_LEVELS.Contains(input))
+        if (LEVEL_PASSWORDS.ContainsKey(input))
         {
             level = int.Parse(input);
             startLevel();
@@ -60,8 +67,20 @@ Enter your selection:");
     private void startLevel()
     {
         screen = Screen.Password;
-        Terminal.WriteLine($"You've chosen the level {level}.");
+        Terminal.WriteLine($"You've chosen level {level}.");
         Terminal.WriteLine("Please enter your password:");
+    }
+
+    private void checkPassword(string input)
+    {
+        if (LEVEL_PASSWORDS[level.ToString()] == input)
+        {
+            Terminal.WriteLine("Congratulations.");
+        }
+        else
+        {
+            Terminal.WriteLine("Invalid password, try again.");
+        }
     }
 
     enum Screen
