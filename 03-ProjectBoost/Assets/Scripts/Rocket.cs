5e13a2e52632f937336df81b885a4db5886f6bc5
RunninglVlan <RedCapShortCut@gmail.com>
2018-11-06 14:29:36 +0200

    Added Rigidbody to the Rocket and the script that moves it   Moved script into folder   Changed mass and angularDrag a bit   Made rocket freeze on X/Y rotation   Added thrust and torque parameters

---
+++ 03-ProjectBoost/Assets/Scripts/Rocket.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    private const string THRUST_BUTTON = "Jump";
+
+    [SerializeField] int thrust = 20;
+    [SerializeField] int torque = 10;
+
+    new private Rigidbody rigidbody;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        move();
+    }
+
+    private void move()
+    {
+        if (Input.GetButton(THRUST_BUTTON))
+        {
+            rigidbody.AddRelativeForce(Vector3.up * thrust);
+        }
+        rigidbody.AddTorque(Vector3.back * torque * Input.GetAxis("Horizontal"));
+    }
+}
---
+++ 03-ProjectBoost/README.md
@@ -0,0 +1,4 @@
+# Notes
+## What's new
+- [Debug Inspector Mode](https://docs.unity3d.com/Manual/InspectorOptions.html) lets you see private variables too (although their values can't be edited like those of public variables)
+- Use Rigidbody's [AddRelativeForce](https://docs.unity3d.com/ScriptReference/Rigidbody.AddRelativeForce.html) to add a force relative to its coordinate system.
