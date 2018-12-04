5a03058d6c79a5c39cf062b5b48d85e9db96ab1a
RunninglVlan <RedCapShortCut@gmail.com>
2018-12-04 12:27:08 +0200

    Created Oscillator with manual movement   Attached it to one obstacle

---
+++ 03-ProjectBoost/Assets/Scripts/Oscillator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    [SerializeField] Vector3 movementVector;
+    [Range(0, 1)]
+    [SerializeField] float movementFactor;
+
+    private Vector3 startPosition;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        var offset = movementFactor * movementVector;
+        transform.position = startPosition + offset;
+    }
+}
