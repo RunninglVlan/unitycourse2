7d1af36f5e0b131d147e4ed5e3893ee6ef7017fe
RunninglVlan <RedCapShortCut@gmail.com>
2018-12-04 13:51:26 +0200

    Made oscillation automatic using Sin   Prevented division by zero using Max   tau is number of radians in 1 turn   movementFactor is still [0, 1], unlike Sin [-1, 1]   Added cycleTime to control speed of oscillation

--- 03-ProjectBoost/Assets/Scripts/Oscillator.cs
+++ 03-ProjectBoost/Assets/Scripts/Oscillator.cs
@@ -4,9 +4,8 @@ using UnityEngine;
 
 public class Oscillator : MonoBehaviour
 {
-    [SerializeField] Vector3 movementVector;
-    [Range(0, 1)]
-    [SerializeField] float movementFactor;
+    [SerializeField] Vector3 movementVector = new Vector3(7, 0, 0);
+    [SerializeField] float cycleTime = 2;
 
     private Vector3 startPosition;
 
@@ -17,6 +16,10 @@ public class Oscillator : MonoBehaviour
 
     void Update()
     {
+        var cycles = Time.time / Mathf.Max(cycleTime, 0.1f);
+        const float tau = Mathf.PI * 2;
+        var sin = Mathf.Sin(cycles * tau);
+        var movementFactor = sin / 2 + 0.5f;
         var offset = movementFactor * movementVector;
         transform.position = startPosition + offset;
     }
