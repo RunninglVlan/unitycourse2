986c47eea50327d51c71a26f25a36840a6dc76c6
RunninglVlan <RedCapShortCut@gmail.com>
2019-01-17 14:07:57 +0200

    Made Music Player Singleton   Added Singleton abstract class   Upgraded to latest Unity Version

---
+++ 04-ArgonAssault/Assets/Scripts/Singleton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Singleton : MonoBehaviour
+{
+    void Awake()
+    {
+        ensureSingleton();
+    }
+
+    private void ensureSingleton()
+    {
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
