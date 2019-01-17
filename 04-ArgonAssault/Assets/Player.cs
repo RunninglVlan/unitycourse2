fee6f9c39ffe09490abf3a3f107452787675ec63
RunninglVlan <RedCapShortCut@gmail.com>
2019-01-17 18:39:09 +0200

    Made Player move using Horizontal and Vertical axes   By changing its localPosition

---
+++ 04-ArgonAssault/Assets/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] int movementSpeed = 10;
+
+    void Update()
+    {
+        move();
+    }
+
+    private void move()
+    {
+        var delta = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        delta.Normalize();
+        delta *= movementSpeed * Time.deltaTime;
+        var newPosition = transform.localPosition + delta;
+        transform.localPosition = newPosition;
+    }
+}
