1e7f413d493b2de2d32759a23760e064dadcc4c5
RunninglVlan <RedCapShortCut@gmail.com>
2019-01-18 12:42:50 +0200

    Made Player move inside movement boundaries   Calculated them based on viewport and ship extents   Resolution change is somehow supported     Created special camera which doesn't rotate in game for that

---
+++ 04-ArgonAssault/Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] Camera resolutionCamera;
+    [SerializeField] int movementSpeed = 10;
+    [SerializeField] Vector2 extents = Vector2.one;
+
+    private float xBoundary;
+    private float yBoundary;
+    private Vector2 resolution;
+
+    void Start()
+    {
+        resolution = new Vector2(Screen.width, Screen.height);
+        setUpMovementBoundaries();
+    }
+
+    private void setUpMovementBoundaries()
+    {
+        var zeroBoundary = resolutionCamera.ViewportToWorldPoint(new Vector3(0, 0, transform.localPosition.z));
+        var oneBoundary = resolutionCamera.ViewportToWorldPoint(new Vector3(1, 1, transform.localPosition.z));
+        xBoundary = (oneBoundary.x - zeroBoundary.x) / 2 - extents.x;
+        yBoundary = (oneBoundary.y - zeroBoundary.y) / 2 - extents.y;
+    }
+
+    void Update()
+    {
+        if (resolution.x != Screen.width || resolution.y != Screen.height)
+        {
+            setUpMovementBoundaries();
+            resolution = new Vector2(Screen.width, Screen.height);
+        }
+        move();
+    }
+
+    private void move()
+    {
+        var delta = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        delta.Normalize();
+        delta *= movementSpeed * Time.deltaTime;
+        var newPosition = transform.localPosition + delta;
+        newPosition.x = Mathf.Clamp(newPosition.x, -xBoundary, xBoundary);
+        newPosition.y = Mathf.Clamp(newPosition.y, -yBoundary, yBoundary);
+        transform.localPosition = newPosition;
+    }
+}
