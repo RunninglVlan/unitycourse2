ae5ccc6f933759acc57accff7806ab388105e924
RunninglVlan <RedCapShortCut@gmail.com>
2019-01-22 14:26:02 +0200

    Implemented rotation of ship using position and control flow   Position is factored to set pitch and yaw     So that ship wouldn't aim at the center of the screen   Pitch and Roll are changed based on control throw     To make smooth movements based on Input axes   Refactoring     Renamed delta to controlFlow, made it a property     Separated offset and normalized throw from delta       As controlFlow is now also used in rotation         it shouldn't be normalized   Tweaked script properties a bit   Grouped rotation properties

--- 04-ArgonAssault/Assets/Scripts/Player.cs
+++ 04-ArgonAssault/Assets/Scripts/Player.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] Camera resolutionCamera;
-    [SerializeField] int movementSpeed = 10;
+    [SerializeField] int movementSpeed = 20;
     [SerializeField] Vector2 extents = Vector2.one;
 
+    [Header("Rotation")]
+    [SerializeField] float positionPitchFactor = -2;
+    [SerializeField] float throwPitchFactor = -20;
+    [SerializeField] float positionYawFactor = 2;
+    [SerializeField] float throwRollFactor = -20;
+
+    private Vector2 resolution;
     private float xBoundary;
     private float yBoundary;
-    private Vector2 resolution;
+    private Vector3 controlThrow;
 
     void Start()
     {
@@ -30,6 +37,7 @@ public class Player : MonoBehaviour
     {
         processResolutionChange();
         move();
+        rotate();
     }
 
     private void processResolutionChange()
@@ -43,12 +51,22 @@ public class Player : MonoBehaviour
 
     private void move()
     {
-        var delta = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        delta.Normalize();
-        delta *= movementSpeed * Time.deltaTime;
-        var newPosition = transform.localPosition + delta;
+        controlThrow = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        var nomalizedThrow = controlThrow;
+        nomalizedThrow.Normalize();
+        var offset = nomalizedThrow * movementSpeed * Time.deltaTime;
+        var newPosition = transform.localPosition + offset;
         newPosition.x = Mathf.Clamp(newPosition.x, -xBoundary, xBoundary);
         newPosition.y = Mathf.Clamp(newPosition.y, -yBoundary, yBoundary);
         transform.localPosition = newPosition;
+    }
+
+    private void rotate()
+    {
+        var pitch = transform.localPosition.y * positionPitchFactor;
+        pitch += controlThrow.y * throwPitchFactor;
+        var yaw = transform.localPosition.x * positionYawFactor;
+        var roll = controlThrow.x * throwRollFactor;
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
 }
