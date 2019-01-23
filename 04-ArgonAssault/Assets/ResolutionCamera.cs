56b54b49fe904b117585b00f86ef63a5b2e0944f
RunninglVlan <RedCapShortCut@gmail.com>
2019-01-23 13:28:58 +0200

    Refactoring - extracted resolution change code from player to camera

---
+++ 04-ArgonAssault/Assets/ResolutionCamera.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResolutionCamera : MonoBehaviour
+{
+    private new Camera camera;
+    private Vector2 resolution = Vector2.zero;
+    private Player player;
+
+    void Awake()
+    {
+        camera = GetComponent<Camera>();
+        camera.fieldOfView = Camera.main.fieldOfView;
+    }
+
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+    }
+
+    void Update()
+    {
+        processResolutionChange();
+    }
+
+    private void processResolutionChange()
+    {
+        if (resolution.x != Screen.width || resolution.y != Screen.height)
+        {
+            var playerPositionZ = player.gameObject.transform.localPosition.z;
+            var zeroBoundary = camera.ViewportToWorldPoint(new Vector3(0, 0, playerPositionZ));
+            var oneBoundary = camera.ViewportToWorldPoint(new Vector3(1, 1, playerPositionZ));
+            player.setUpMovementBoundaries(zeroBoundary, oneBoundary);
+
+            resolution = new Vector2(Screen.width, Screen.height);
+        }
+    }
+}
--- 04-ArgonAssault/Assets/Scripts/Player.cs
+++ 04-ArgonAssault/Assets/Scripts/Player.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] Camera resolutionCamera;
     [SerializeField] int movementSpeed = 20;
     [SerializeField] Vector2 extents = Vector2.one;
 
@@ -14,44 +13,20 @@ public class Player : MonoBehaviour
     [SerializeField] float positionYawFactor = 2;
     [SerializeField] float throwRollFactor = -20;
 
-    private Vector2 resolution;
     private float xBoundary;
     private float yBoundary;
     private Vector3 controlThrow;
 
-    void Awake()
+    public void setUpMovementBoundaries(Vector3 zeroBoundary, Vector3 oneBoundary)
     {
-        resolutionCamera.fieldOfView = Camera.main.fieldOfView;
-    }
-
-    void Start()
-    {
-        resolution = new Vector2(Screen.width, Screen.height);
-        setUpMovementBoundaries();
-    }
-
-    private void setUpMovementBoundaries()
-    {
-        var zeroBoundary = resolutionCamera.ViewportToWorldPoint(new Vector3(0, 0, transform.localPosition.z));
-        var oneBoundary = resolutionCamera.ViewportToWorldPoint(new Vector3(1, 1, transform.localPosition.z));
         xBoundary = (oneBoundary.x - zeroBoundary.x) / 2 - extents.x;
         yBoundary = (oneBoundary.y - zeroBoundary.y) / 2 - extents.y;
     }
 
     void Update()
     {
-        processResolutionChange();
         move();
         rotate();
-    }
-
-    private void processResolutionChange()
-    {
-        if (resolution.x != Screen.width || resolution.y != Screen.height)
-        {
-            resolution = new Vector2(Screen.width, Screen.height);
-            setUpMovementBoundaries();
-        }
     }
 
     private void move()
